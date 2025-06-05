0e866eb7eca9086f5830b0ad368cdba85dba2b69
Syed Academy <billyxkhan98@gmail.com>
2025-06-05 18:53:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a class="text-2xl

--- src/components/eaders-logo.tsx
+++ src/components/eaders-logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const EadersLogo = () => {
   return (
     <Link href="/" className="text-2xl font-headline font-bold text-primary hover:text-primary/90 transition-colors">
-      Eaders
+      Eaders <sub className="text-xs font-medium">by Nizami LTD</sub>
     </Link>
   );
 };
